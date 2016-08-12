10d420707df5f5eaf808a6d49354c888e797d886
Kristine Sumiyoshi <kristine.dasalla@gmail.com>
2016-08-11 22:54:55 -0700

    Fixed Checkpoint 30 issue with displaying song names in album

--- scripts/album.js
+++ scripts/album.js
@@ -48,15 +48,21 @@ var albumSanFrancisco = {
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
-       '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-     + '  <td class="song-item-title">' + songName + '</td>'
-     + '  <td class="song-item-duration">' + songLength + '</td>'
-     + '</tr>'
-     ;
+        '<tr class="album-view-song-item">'
+    +'  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+    +'  <td class="song-item-title">' + songName + '</td>'
+    +'  <td class="song-item-duration">' + songLength + '</td>'
+    +'</tr>'
+    ;
     
     return $(template);
 };
+
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
      var $albumTitle = $('.album-view-title');
@@ -64,22 +70,21 @@ var setCurrentAlbum = function(album) {
      var $albumReleaseInfo = $('.album-view-release-info');
      var $albumImage = $('.album-cover-art');
      var $albumSongList = $('.album-view-song-list');
-    
-     $albumTitle.text(album.title);
+    //#2
+     $albumTitle.text(album.name);
      $albumArtist.text(album.artist);
      $albumReleaseInfo.text(album.year + ' ' + album.label);
      $albumImage.attr('src', album.albumArtUrl);
     
-<<<<<<< HEAD
+    //#3
      $albumSongList.empty();
-=======
-    $albumSongList.empty();
->>>>>>> checkpoint-30-jquery-collection-album-views
     
+    //#4
     for (i = 0; i < album.songs.length; i++) {
-         var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-         $albumSongList.append($newRow);
+        var $newRow = createSongRow(i + 1,      album.songs[i].name, album.songs[i].length);
+        $albumSongList.append($newRow);
     }
+    
 };
 
 
